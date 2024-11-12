2b738f99f3be03f216454aefe360978540b9dc5e
Siddhant Pallod <68698846+siddhantpallod@users.noreply.github.com>
2024-11-12 12:03:40 -0500

    Add files via upload

--- whackamole.py
+++ whackamole.py
@@ -1,20 +1,44 @@
 import pygame
+import random
 
+GRID_WIDTH = 20
+GRID_HEIGHT = 16
+SQUARE_SIZE = 32
+
+def draw_grid(screen):
+    for x in range(GRID_WIDTH + 1):
+        pygame.draw.line(screen, "dark green", (x * SQUARE_SIZE, 0), (x * SQUARE_SIZE, 512))
+
+    for y in range(GRID_WIDTH + 1):
+        pygame.draw.line(screen, "dark green", (0, y * SQUARE_SIZE), (640, y * SQUARE_SIZE))
+
+def move_mole(mole_rect):
+    random_x = random.randrange(0, GRID_WIDTH) * SQUARE_SIZE
+    random_y = random.randrange(0, GRID_HEIGHT) * SQUARE_SIZE
+    mole_rect.topleft = (random_x, random_y)
 
 def main():
     try:
         pygame.init()
-        # You can draw the mole with this snippet:
-        # screen.blit(mole_image, mole_image.get_rect(topleft=(x,y)))
-        mole_image = pygame.image.load("mole.png")
+
         screen = pygame.display.set_mode((640, 512))
+        pygame.display.set_caption("Whack-a-mole")
+        mole_image = pygame.image.load("mole.png")
+        mole_rect = mole_image.get_rect(topleft = (0, 0))
+
         clock = pygame.time.Clock()
         running = True
         while running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    if mole_rect.collidepoint(event.pos):
+                        move_mole(mole_rect)
+
             screen.fill("light green")
+            draw_grid(screen)
+            screen.blit(mole_image, mole_rect)
             pygame.display.flip()
             clock.tick(60)
     finally:
